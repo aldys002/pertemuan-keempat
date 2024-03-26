bc727213cd774c8b23979cb6ee35223f3b2c9240
aldys002 <igidiaaldys@gmail.com>
2024-03-26 15:03:59 +0700

    menambah case 2

--- fungsiswitch.cpp
+++ fungsiswitch.cpp
@@ -19,5 +19,32 @@ int penjumlahan (int a, int b)
 
 int main()
 {
+    int pilihan;
+
+    do
+    {
+        cout << "kalkulator sederhana" <<endl;
+        cout << "====================" <<endl;
+        cout << "1. penjumlahan" <<endl;
+        cout << "2. pengurangan" <<endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl;
+        cout << "5. keluar" <<endl;
+        cout << "====================" <<endl;
+        cin >> pilihan;
+
+        switch (pilihan)
+
+        case 1:
+            inputData();
+            cout << "Hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) <<endl;
+            break;
+        case 2:
+             inputData();
+             cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) <<endl;
+             break;
+
+    } while (/* condition */);
+    
     
 }
